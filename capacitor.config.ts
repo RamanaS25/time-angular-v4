4197619b33c1b1914e0e1cdfb60598f98e3134a1
ramana naicker <ramanas25n@gmail.com>
2023-12-10 15:35:05 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'time-angular-v4',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
